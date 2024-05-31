93be0ddfe9cc0b0d1f6399fade7329d9eebbb4e2
Sébastien Gallou <sebastien.gallou@gmail.com>
2024-05-31 21:56:18 +0200

    Cleanup

--- libs/pages/home/src/lib/home.tsx
+++ libs/pages/home/src/lib/home.tsx
@@ -1,21 +1,22 @@
-import { useContext } from "react";
-import { YadomsConnectionContext, YadomsConnection } from 'libs/shared/src/lib/services/YadomsWebSocketConnection'
-import KeywordLog from "./widgets/ybutton";
+import { useContext } from 'react';
+import { YadomsConnectionContext, YadomsConnection } from '@yadoms/shared';
+import KeywordLog from './widgets/ybutton';
 
 /* eslint-disable-next-line */
-export interface PagesHomeProps { }
-
+export interface PagesHomeProps {}
 
 export function Home(props: PagesHomeProps) {
-  const { connected, serverCurrentTime } = useContext(YadomsConnectionContext) as YadomsConnection;
+  const { connected, serverCurrentTime } = useContext(
+    YadomsConnectionContext
+  ) as YadomsConnection;
 
   return (
     <div>
       <h1>Welcome to PagesHome!</h1>
-      <p>Socket is {connected ? "connected" : "DISCONNECTED"}</p>
+      <p>Socket is {connected ? 'connected' : 'DISCONNECTED'}</p>
       <p>Server current time is {serverCurrentTime?.toString()}</p>
-      <KeywordLog widgetId={1} keywordsToListen={"45,46"}></KeywordLog>
-      <KeywordLog widgetId={2} keywordsToListen={"24, 25, 26"}></KeywordLog>
+      <KeywordLog widgetId={1} keywordsToListen={'45,46'}></KeywordLog>
+      <KeywordLog widgetId={2} keywordsToListen={'24, 25, 26'}></KeywordLog>
     </div>
   );
 }
--- libs/shared/src/lib/services/YadomsWebSocketConnection.tsx
+++ libs/shared/src/lib/services/YadomsWebSocketConnection.tsx
@@ -12,9 +12,8 @@ export interface AcquisitionListener {
 }
 
 class YadomsWebSocketConnection {
-
   private _ws: WebSocket;
-  private _connected: boolean = false;
+  private _connected = false;
   private _acquisitionsListeners = new Map<number, AcquisitionListener[]>();
 
   constructor() {
@@ -22,111 +21,131 @@ class YadomsWebSocketConnection {
 
     this._ws.onopen = () => {
       this._connected = true;
-      this.onConnected?.(this._connected)
+      this.onConnected?.(this._connected);
       this.updateKeywordsFilter();
-    }
+    };
 
     this._ws.onclose = () => {
       this._connected = false;
-      this.onConnected?.(this._connected)
-    }
+      this.onConnected?.(this._connected);
+    };
 
     this._ws.onmessage = (event: any) => {
       const data = JSON.parse(event.data);
-      if ("serverCurrentTime" in data) {
+      if ('serverCurrentTime' in data) {
         this.onServerCurrenTime?.(this.parseYadomsDate(data.serverCurrentTime));
         return;
       }
-      if ("newAcquisition" in data) {
+      if ('newAcquisition' in data) {
         const newAcquisition: Acquisition = {
-          date: this.parseYadomsDate(data.newAcquisition.date), keyword: data.newAcquisition.keywordId, value: data.newAcquisition.value
+          date: this.parseYadomsDate(data.newAcquisition.date),
+          keyword: data.newAcquisition.keywordId,
+          value: data.newAcquisition.value,
         };
-        this._acquisitionsListeners.get(newAcquisition.keyword)?.forEach((listener) => {
-          listener.onReceived(newAcquisition)
-        })
+        this._acquisitionsListeners
+          .get(newAcquisition.keyword)
+          ?.forEach((listener) => {
+            listener.onReceived(newAcquisition);
+          });
         return;
       }
-      console.warn("Unknown socket message received : " + event);
-    }
+      console.warn('Unknown socket message received : ' + event);
+    };
   }
 
-  subscribeToKeywordAcquisitions(keywords: number[], newListener: AcquisitionListener) {
-
+  subscribeToKeywordAcquisitions(
+    keywords: number[],
+    newListener: AcquisitionListener
+  ) {
     // Cleanup previous listeners
-    this._acquisitionsListeners.forEach((listeners: AcquisitionListener[], key: number) => {
-      listeners.forEach((listener: AcquisitionListener, listenerIndex: number) => {
-        if (listener.uuid === newListener.uuid)
-          listeners.splice(listenerIndex, 1);
-      });
-    });
+    this._acquisitionsListeners.forEach(
+      (listeners: AcquisitionListener[], key: number) => {
+        listeners.forEach(
+          (listener: AcquisitionListener, listenerIndex: number) => {
+            if (listener.uuid === newListener.uuid)
+              listeners.splice(listenerIndex, 1);
+          }
+        );
+      }
+    );
 
     keywords.forEach((keyword: number) => {
       const listeners = this._acquisitionsListeners.get(keyword);
       if (listeners === undefined)
         this._acquisitionsListeners.set(keyword, [newListener]);
-      else
-        listeners.push(newListener);
+      else listeners.push(newListener);
     });
 
-    if (this._connected)
-      this.updateKeywordsFilter();
+    if (this._connected) this.updateKeywordsFilter();
   }
 
   private updateKeywordsFilter() {
-    if (this._acquisitionsListeners.size == 0)
-      return;
+    if (this._acquisitionsListeners.size == 0) return;
     this.filterAcquisitions([...this._acquisitionsListeners.keys()]);
   }
 
   private filterAcquisitions(keywords: number[]) {
-    this._ws.send(JSON.stringify({ "acquisitionFilter": { "keywords": keywords } }));
+    this._ws.send(
+      JSON.stringify({ acquisitionFilter: { keywords: keywords } })
+    );
   }
 
   private parseYadomsDate(dateAsString: string): Date {
-    return new Date(dateAsString.replace(/([0-9]{4})([0-9]{2})([0-9]{2})T([0-9]{2})([0-9]{2})([0-9]{2}).([0-9]*)/, "$1-$2-$3T$4:$5:$6.$7"));
+    return new Date(
+      dateAsString.replace(
+        /([0-9]{4})([0-9]{2})([0-9]{2})T([0-9]{2})([0-9]{2})([0-9]{2}).([0-9]*)/,
+        '$1-$2-$3T$4:$5:$6.$7'
+      )
+    );
   }
 
   onConnected: ((connected: boolean) => void) | undefined;
   onServerCurrenTime: ((serverTime: Date) => void) | undefined;
 }
 
-
-
 export type YadomsConnection = {
   connected: boolean;
   serverCurrentTime: Date | undefined;
-  subscribeToKeywordAcquisitions: (keywords: number[], onNewAcquisition: AcquisitionListener) => void;
-}
-export const YadomsConnectionContext = createContext<YadomsConnection | null>(null);
+  subscribeToKeywordAcquisitions: (
+    keywords: number[],
+    onNewAcquisition: AcquisitionListener
+  ) => void;
+};
+export const YadomsConnectionContext = createContext<YadomsConnection | null>(
+  null
+);
 
 export const YadomsConnectionContextProvider = ({ children }: any) => {
   const [connected, setConnected] = useState<boolean>(false);
   const [serverCurrentTime, setServerCurrentTime] = useState<Date>();
 
-  function subscribeToKeywordAcquisitions(keywords: number[], onNewAcquisition: AcquisitionListener): void {
+  function subscribeToKeywordAcquisitions(
+    keywords: number[],
+    onNewAcquisition: AcquisitionListener
+  ): void {
     ws.current?.subscribeToKeywordAcquisitions(keywords, onNewAcquisition);
   }
 
   const ws = useRef<YadomsWebSocketConnection | null>(null);
   useEffect(() => {
     const yadomsWebSocketConnection = new YadomsWebSocketConnection();
-    yadomsWebSocketConnection.onConnected = (connected: boolean) => setConnected(connected);
-    yadomsWebSocketConnection.onServerCurrenTime = (serverTime: Date) => setServerCurrentTime(serverTime);
+    yadomsWebSocketConnection.onConnected = (connected: boolean) =>
+      setConnected(connected);
+    yadomsWebSocketConnection.onServerCurrenTime = (serverTime: Date) =>
+      setServerCurrentTime(serverTime);
 
     ws.current = yadomsWebSocketConnection;
   }, []);
 
   return (
-    <YadomsConnectionContext.Provider value={{
-      connected: connected,
-      serverCurrentTime: serverCurrentTime,
-      subscribeToKeywordAcquisitions: subscribeToKeywordAcquisitions
-    }}>
+    <YadomsConnectionContext.Provider
+      value={{
+        connected: connected,
+        serverCurrentTime: serverCurrentTime,
+        subscribeToKeywordAcquisitions: subscribeToKeywordAcquisitions,
+      }}
+    >
       {children}
     </YadomsConnectionContext.Provider>
   );
 };
-
-
-
-
