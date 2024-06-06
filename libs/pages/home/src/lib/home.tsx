75d89f5037392b0a6d70cdf6109d76c92b2d4ea4
Sébastien Gallou <sebastien.gallou@gmail.com>
2024-06-06 22:16:40 +0200

    Create button widget (WIP)

--- libs/pages/home/src/lib/home.tsx
+++ libs/pages/home/src/lib/home.tsx
@@ -1,6 +1,7 @@
 import { useContext } from 'react';
 import { YadomsConnectionContext, YadomsConnection } from '@yadoms/shared';
-import KeywordLog from './widgets/ybutton';
+import YButton from './widgets/YButton';
+import KeywordLog from './widgets/keywordLog';
 
 /* eslint-disable-next-line */
 export interface PagesHomeProps {}
@@ -15,8 +16,10 @@ export function Home(props: PagesHomeProps) {
       <h1>Welcome to PagesHome!</h1>
       <p>Socket is {connected ? 'connected' : 'DISCONNECTED'}</p>
       <p>Server current time is {serverCurrentTime?.toString()}</p>
-      <KeywordLog widgetId={1} keywordsToListen={'45,46'}></KeywordLog>
-      <KeywordLog widgetId={2} keywordsToListen={'24, 25, 26'}></KeywordLog>
+      <YButton widgetId={1} buttonKeyword={20}></YButton>
+      <YButton widgetId={2} buttonKeyword={30}></YButton>
+      <KeywordLog widgetId={3} keywordsToListen={'45,46'}></KeywordLog>
+      <KeywordLog widgetId={4} keywordsToListen={'24, 25, 26'}></KeywordLog>
     </div>
   );
 }
--- libs/pages/home/src/lib/widgets/ybutton.tsx
+++ libs/pages/home/src/lib/widgets/ybutton.tsx
@@ -5,19 +5,20 @@ import {
   AcquisitionListener,
 } from '@yadoms/shared';
 import { v4 as uuidv4 } from 'uuid';
-import { Button, TextInput } from '@mantine/core';
+import { Button } from '@mantine/core';
 import { WidgetProps } from './Widget';
 
-export interface KeywordLogProps extends WidgetProps {
-  keywordsToListen: string;
+export interface ButtonProps extends WidgetProps {
+  buttonKeyword: number;
 }
 
-interface KeywordLogState {
-  myAcquisitions: Acquisition[];
-  keywordsToListen: string;
+interface ButtonState {
+  myAcquisitions: Acquisition[]; //TODO conserver ?
+  buttonKeyword: number;
+  isPressed: boolean;
 }
 
-class KeywordLogAcquisitionListener implements AcquisitionListener {
+class ButtonStateAcquisitionListener implements AcquisitionListener {
   constructor(onNewAcquisition: (newAcquisition: Acquisition) => void) {
     this.uuid = uuidv4();
     this.onNewAcquisition = onNewAcquisition;
@@ -29,81 +30,65 @@ class KeywordLogAcquisitionListener implements AcquisitionListener {
   private onNewAcquisition: (newAcquisition: Acquisition) => void;
 }
 
-class KeywordLog extends Component<KeywordLogProps, KeywordLogState> {
+class YButton extends Component<ButtonProps, ButtonState> {
   static contextType = YadomsConnectionContext;
   context!: React.ContextType<typeof YadomsConnectionContext>;
 
-  acquisitionListener: KeywordLogAcquisitionListener;
+  acquisitionListener: ButtonStateAcquisitionListener;
 
-  constructor(props: KeywordLogProps) {
+  constructor(props: ButtonProps) {
     super(props);
-    console.log('KeywordLog creation #' + props.widgetId);
+    console.log('yButton creation #' + props.widgetId);
 
     this.state = {
       myAcquisitions: [],
-      keywordsToListen: props.keywordsToListen,
+      buttonKeyword: props.buttonKeyword,
+      isPressed: false
     };
 
-    this.acquisitionListener = new KeywordLogAcquisitionListener(
+    this.acquisitionListener = new ButtonStateAcquisitionListener(
       this.onNewAcquisition.bind(this)
     );
 
     this.onNewAcquisition = this.onNewAcquisition.bind(this);
     this.applyKeywordsToListen = this.applyKeywordsToListen.bind(this);
-    this.handleKeywordsToListenChange =
-      this.handleKeywordsToListenChange.bind(this);
+    this.onClick = this.onClick.bind(this);
   }
 
   componentDidMount() {
     this.applyKeywordsToListen();
   }
 
-  private parseKeywordsToListen(value: string) {
-    return value.split(',').map((element) => {
-      return parseInt(element, 10);
-    });
-  }
-
   private onNewAcquisition(newAcquisition: Acquisition) {
-    this.setState((prevState) => ({
-      myAcquisitions: [newAcquisition, ...prevState.myAcquisitions].slice(0, 4),
-    }));
+    this.setState({
+      isPressed: parseInt(newAcquisition.value) != 0 ? true : false,
+    });
   }
 
   private applyKeywordsToListen() {
     this.context!.subscribeToKeywordAcquisitions(
-      this.parseKeywordsToListen(this.state.keywordsToListen),
+      [this.state.buttonKeyword],
       this.acquisitionListener
     );
   }
 
-  private handleKeywordsToListenChange(
-    event: React.ChangeEvent<HTMLInputElement>
-  ) {
-    this.setState({ keywordsToListen: event.currentTarget.value });
+  private onClick() {
+    console.log("Button clicked !") //TODO faire l'action quand même !
+    this.setState({
+      isPressed: !this.state.isPressed,
+    });
   }
 
   render() {
     return (
       <div style={{ border: '1px solid', margin: '10px' }}>
-        <h2>yButton #{this.props.widgetId}</h2>
-        <TextInput
-          data-autofocus
-          label="Select keywords to listen (comma separated)"
-          value={this.state.keywordsToListen}
-          onChange={this.handleKeywordsToListenChange}
-        />
-        <Button onClick={this.applyKeywordsToListen} type="submit">
-          Apply
+        <h2>Button #{this.props.widgetId} on keyword #{this.props.buttonKeyword}</h2>
+        <Button onClick={this.onClick} className={`toggle-button ${this.state.isPressed ? 'on' : 'off'}`}>
+          Click on Me ! {this.state.isPressed ? 'ON' : 'OFF'}
         </Button>
-        {this.state.myAcquisitions.map((acq) => (
-          <p key={uuidv4()}>
-            [{acq.date.toLocaleTimeString()}] kwd #{acq.keyword} = {acq.value}
-          </p>
-        ))}
       </div>
     );
   }
 }
 
-export default KeywordLog;
+export default YButton;
