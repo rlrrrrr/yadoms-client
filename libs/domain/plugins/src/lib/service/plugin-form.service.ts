e16e58948af5e5395ec82e519792ebfe62073fc0
oussama Dahmaz <dahmaz@MacBook-Pro-de-odahmaz.local>
2024-07-31 20:56:40 +0200

    add boolean in nestedSectionFields

--- libs/domain/plugins/src/lib/service/plugin-form.service.ts
+++ libs/domain/plugins/src/lib/service/plugin-form.service.ts
@@ -184,6 +184,13 @@ export const getNestedSectionFields = (
           field: field,
         });
         break;
+      case PluginConfigurationSchemaType.Boolean:
+        newInitialValues.push({
+          key: key,
+          path: `${parentKey}.content.${key}.value`,
+          field: field,
+        });
+        break;
       case PluginConfigurationSchemaType.Enum:
         newInitialValues.push({
           key: key,
